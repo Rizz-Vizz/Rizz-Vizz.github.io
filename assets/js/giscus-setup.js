924812014151839b880e5833749b5130819386d3
Rizz-Vizz <ritviz.aggarwal@proton.me>
2026-02-10 13:38:57 +0000

    Deploying to gh-pages from @ Rizz-Vizz/Rizz-Vizz.github.io@7a4112ef938ba83efc3856db14f794465dc70e29 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Rizz-Vizz/rizzvizz.github.io",
+    "data-repo": "Rizz-Vizz/Rizz-Vizz.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
